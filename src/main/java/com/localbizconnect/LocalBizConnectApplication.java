9fb5bb93f3326752532ee04af6df177c06c1fb5d
Daniel Agudelo <d.agudelo816@gmai.com>
2024-01-02 23:24:38 -0500

    first commit

---
+++ src/main/java/com/localbizconnect/LocalBizConnectApplication.java
@@ -0,0 +1,13 @@
+package com.localbizconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBizConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalBizConnectApplication.class, args);
+	}
+
+}
